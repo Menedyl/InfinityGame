77a4d4de876de1750aa89c7834b5e9747143f8f8
Menedyl <nicolas.bostjancic@gmail.com>
2020-05-20 17:29:58 +0200

    Create of the world base
    - add World object
    - add Region object
    - add Area object

---
+++ Assets/Script/World.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public Region prefRegion;
+
+    private void Start()
+    {
+        Region region = Instantiate(prefRegion, transform);
+        region.GetComponent<RectTransform>().anchoredPosition = new Vector2(-10, -10);
+
+    }
+}
