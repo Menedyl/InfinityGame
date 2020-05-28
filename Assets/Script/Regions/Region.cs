e39813b380c3017623d5efadfa117b940f278226
Menedyl <nicolas.bostjancic@gmail.com>
2020-05-28 19:17:13 +0200

    Add first entity Human
    - Create object "Human"
    - Add spawn for entity "Human"
    - Add movement for entity "Human"
    - Rework movement for player
    - Rework generation of ground

---
+++ Assets/Script/Regions/Region.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class Region : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _prefArea;
+    [SerializeField]
+    private GameObject _prefBorder;
+    [SerializeField]
+    private GameObject _prefHuman;
+
+    private Transform _areasTransform;
+    private Transform _bordersTransform;
+
+    private static Border[] _borders;
+    public readonly int size = 100;
+
+    private void Awake()
+    {
+        _areasTransform = transform.Find("Areas");
+        _bordersTransform = transform.Find("Borders");
+        CreateGround();
+        Region._borders = _bordersTransform.GetComponentsInChildren<Border>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnEntity", 1, 3);
+    }
+
+    private void CreateGround()
+    {
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                GameObject ground;
+                if (x == 0 || x == (size - 1) || y == 0 || y == (size - 1))
+                {
+                    ground = Instantiate(_prefBorder, _bordersTransform);
+                }
+                else
+                {
+                    ground = Instantiate(_prefArea, _areasTransform);
+                }
+                ground.transform.position = new Vector3(x, y, 0);
+            }
+        }
+    }
+
+    private void SpawnEntity()
+    {
+        Vector3 pos = Region.GetRandomBorderPos();
+        Instantiate(_prefHuman, pos, Quaternion.identity);
+    }
+
+    public static Vector3 GetRandomBorderPos()
+    {
+        return Region._borders[Random.Range(0, Region._borders.Length)].transform.position;
+    }
+
+}
